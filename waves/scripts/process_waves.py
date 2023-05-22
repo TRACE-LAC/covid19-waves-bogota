fe7a3228abdabb8647d71c42250e1db55c15e279
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2023-05-22 12:59:53 +0000

    run: results waves

--- waves/scripts/process_waves.py
+++ waves/scripts/process_waves.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 15 2022
 @author: davidsantiagoquevedo
 @author: ntorresd
 """
 import yaml
 import pandas as pd
-from time import time
-import sys 
 
-ymlfile = open("config.yml", "r")
-cfg = yaml.load(ymlfile)
-config = cfg["default"]
+config = yaml.load(open("config.yml", "r"))["default"]
 
 OUT_PATH = config['PATHS']['OUT_PATH'].format(dir = 'waves')
 # The roots were selected by visual inspection of /figures/roots_confirmed_cases.png 
@@ -29,5 +24,4 @@ df_waves['label'] = ['start', 'end', 'start', 'end', 'start', 'end', 'start', 'e
 df_waves_t = df_waves.pivot(index = 'wave', columns = 'label', values = 'date').reset_index()
 df_waves_t = df_waves_t[['wave', 'start', 'end']]
 df_waves_t.columns = ['wave','start_date','end_date']
-
 df_waves_t[df_waves_t.wave.notnull()].to_csv(OUT_PATH + 'waves.csv', index = False)
